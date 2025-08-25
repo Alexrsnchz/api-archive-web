b8bce1a9b7c273470430fc6c61f521188478dc0d
Alexrsnchz <alexrsnchz@gmail.com>
2025-08-25 19:17:39 +0200

    chore: Add geist font

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@styles/index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@fontsource/geist-sans';
+import '@styles/index.css';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
