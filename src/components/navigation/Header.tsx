b7835b7f1796d96866b330878401446e9afceb50
Alexrsnchz <alexrsnchz@gmail.com>
2025-09-03 12:52:40 +0200

    style: Change header icon color

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <nav className="flex justify-center py-4 md:py-6 lg:py-5 bg-transparent border-b border-neutral-800">
       <div className="flex items-center gap-4">
-        <ArchiveIcon className="size-7 md:size-10 lg:size-9 text-violet-500" />
+        <ArchiveIcon className="size-7 md:size-10 lg:size-9 text-midnight-400" />
         <span className="text-2xl md:text-4xl lg:text-3xl font-bold text-gray-200">
           API Archive
         </span>
