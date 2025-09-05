9e6edc56638c2f07f679c1c1670d0f5b95e0b525
Alexrsnchz <alexrsnchz@gmail.com>
2025-09-05 15:46:53 +0200

    style: Adjust the text size of the hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,13 +2,13 @@ import SearchBar from '@components/search/SearchBar';
 
 export default function Hero() {
   return (
-    <section className="py-8 md:py-10 lg:py-15">
+    <section className="py-12 md:py-20 lg:py-25">
       <div className="mb-8">
-        <h1 className="mb-3 md:mb-4 lg:mb-6 text-center text-3xl md:text-5xl lg:text-6xl font-bold text-neutral-50">
+        <h1 className="mb-3 md:mb-4 lg:mb-6 text-center text-5xl lg:text-6xl font-bold text-neutral-50">
           Discover the Perfect API
         </h1>
 
-        <p className="mx-auto max-w-sm md:max-w-xl lg:max-w-2xl text-center text-sm md:text-lg lg:text-xl text-neutral-400">
+        <p className="mx-auto max-w-sm md:max-w-xl lg:max-w-2xl text-center text-md md:text-lg lg:text-xl text-neutral-400">
           Your modern and simple resource for finding and exploring interesting APIs. Designed to
           make API discovery easy.
         </p>
